374f530e970a07f82c29e37c9ac1063b48018ed3
Ace <Ace@192.168.1.2>
2017-12-03 00:18:10 +0800

    create

---
+++ src/main/java/com/hujao/SamwestApplication.java
@@ -0,0 +1,15 @@
+package com.hujao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@ComponentScan
+@EnableWebMvc
+public class SamwestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SamwestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hujao/po/ArticleEntity.java
@@ -0,0 +1,5 @@
+package com.hujao.po;
+
+public class ArticleEntity {
+
+}

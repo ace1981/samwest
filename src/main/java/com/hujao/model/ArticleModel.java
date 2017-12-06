eb117c9ad4ccd584198d3e21c2c7e5091cc9f669
ace1981 <38688368@qq.com>
2017-12-07 03:03:49 +0800

    1

--- src/main/java/com/hujao/model/ArticleModel.java
+++ src/main/java/com/hujao/model/ArticleModel.java
@@ -2,6 +2,7 @@ package com.hujao.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
 
 import lombok.*;
 @Document(indexName="samwest",type="article",indexStoreType="fs",shards=5,replicas=1,refreshInterval="-1")
